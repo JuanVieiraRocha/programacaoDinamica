7a0fc22935cbc183c80453fff0c5583978302ae7
Juan <danieljruan@gmail.com>
2026-06-30 18:39:01 -0300

    Não consigo resolver

--- divisaoAtividades.py
+++ divisaoAtividades.py
@@ -7,7 +7,6 @@ def gerar_diretorias(quantidade_De_Diretorias):
         diretorias.append(diretoria)
     return diretorias
 def gerar_atividades(quantidade_De_Atividades):
-    rd.seed(3)
     atividades = []
     for i in range(quantidade_De_Atividades):
         atividade = [rd.randint(1, 4), rd.randint(1, 3)]
@@ -23,22 +22,27 @@ def cria_tabela(linhas, colunas):
             
 diretorias = gerar_diretorias(4)
 atividades = gerar_atividades(6)
-tabela = cria_tabela(6, 4)
-print(tabela, '\n',  diretorias, '\n', atividades)
+tabela = cria_tabela(4, 6)
 i = 0
 j = 0
-for atividade in atividades:
-    for diretoria in diretorias:
-        horas_atividade = atividades[i][0]
-        print(horas_atividade)
-        peso_Atividade = atividades[i][1]
-        print(peso_Atividade)
-        if diretoria[0] < horas_atividade:
+for i in range(3):
+    for j in range(6):
+        horas_atividade = atividades[j][0]
+        peso_Atividade = atividades[j][1]
+
+        horas_diretoria = diretorias[i][0]
+        atividades_diretoria = diretorias[i][1]
+        if  horas_diretoria < horas_atividade:
             tabela[i][j] = tabela[i-1][j]
+            
         else:
             atual = peso_Atividade + tabela[i-1][j-horas_atividade]
             anterior = tabela[i -1][j]
-            max(atual, anterior)
-        i += 1
-        j += 1
-tabela[atividades, diretorias]
+            tabela[i][j] = max(atual, anterior)
+            diretorias[i][1].append(atividades[j])
+        print(f'diretoria ${diretorias[i]}, recebeu a atividade ${atividades[j]}. isso custou ${horas_atividade}. Agora a diretoria tem ${diretorias[i][0]} horas \n')
+        print(diretorias[i][1])
+print(tabela[0])
+print(tabela[1])
+print(tabela[2])
+print(tabela[3])
